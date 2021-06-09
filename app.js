2e9316870e6b4164aa213bf3a28e437a21a6e433
teradaian <teradaian@gmail.com>
2021-06-09 15:35:49 -0700

    solve 14

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ Exercise 2:
 
 // Complete Exercise 2 below...
 
-
+foods.push('pizza', 'cheeseburger')
 
 console.log('Exercise 2 Result:\n',  foods);
 
@@ -27,7 +27,7 @@ Exercise 3:
 
 // Complete Exercise 3 below...
 
-
+foods.unshift('taco')
 
 console.log('Exercise 3 Result:\n', foods);
 
@@ -38,7 +38,7 @@ Exercise 4:
 
 // Complete Exercise 4 below...
 
-
+let favFood = foods[1] 
 
 console.log('Exercise 4 Result:\n', favFood);
 
@@ -49,7 +49,7 @@ Exercise 5:
 
 // Complete Exercise 5 below...
 
-
+foods.splice(2, 0, 'tofu')
 
 console.log('Exercise 5 Result:\n', foods);
 
@@ -60,7 +60,7 @@ Exercise 6:
 
 // Complete Exercise 6 below...
 
-
+foods.splice(1, 1, 'sushi', 'cupcake')
 
 console.log('Exercise 6 Result:\n', foods);
 
@@ -72,7 +72,7 @@ Exercise 7:
 
 // Complete Exercise 7 below...
 
-
+let yummy = foods.slice(1, 3)
 
 console.log('Exercise 7 Result:\n', yummy);
 
@@ -83,7 +83,7 @@ Exercise 8:
 
 // Complete Exercise 8 below...
 
-
+let soyIdx = foods.indexOf('tofu')
 
 console.log('Exercise 8 Result:\n', soyIdx);
 
@@ -95,7 +95,7 @@ Exercise 9:
 
 // Complete Exercise 9 below...
 
-
+let allFoods = foods.join(' -> ')
 
 console.log('Exercise 9 Result:\n', allFoods);
 
@@ -106,7 +106,7 @@ Exercise 10:
 
 // Complete Exercise 10 below...
 
-
+let hasSoup = foods.includes('soup') ? true : false;
 
 console.log('Exercise 10 Result:\n', hasSoup);
 
@@ -120,7 +120,13 @@ const nums = [100, 5, 23, 15, 21, 72, 9, 45, 66, 7, 81, 90];
 
 // Complete Exercise 11 below...
 
+let odds = []
 
+nums.forEach((i) => {
+  if(i % 2 === 1){
+    odds.push(i)
+  }
+})
 
 console.log('Exercise 11 Result:\n', odds);
 
@@ -134,7 +140,21 @@ Exercise 12:
 
 // Complete Exercise 12 below...
 
+let fizz =[];
+let buzz = [];
+let fizzbuzz = [];
 
+
+nums.forEach((i) => {
+  if( i % 3 === 0 ){
+    fizz.push(i)
+  } if ( i % 5 === 0 ) {
+    buzz.push(i)
+  } if (i % 3 === 0 && i % 5 === 0) {
+    fizzbuzz.push(i)
+  } 
+  }
+)
 
 console.log('Exercise 12 Results:');
 console.log('  fizz:', fizz);
@@ -157,6 +177,7 @@ const numArrays = [
 // Complete Exercise 13 below...
 
 
+let numList = numArrays.pop(numArrays.length)
 
 console.log('Exercise 13 Result:\n', numList);
 
@@ -167,7 +188,7 @@ Exercise 14:
 
 // Complete Exercise 14 below...
 
-
+let num = numArrays[2][1]
 
 console.log('Exercise 14 Result:\n', num);
 
@@ -178,7 +199,14 @@ Exercise 15:
 */
 
 // Complete Exercise 15 below...
+let total = [];
 
+numArrays.forEach((i) => {
 
+  [i].forEach((j) => {
+    total.push([i][j])
+    })
+    
+})
 
 console.log('Exercise 15 Result:\n', total);
